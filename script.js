d84cde746b8985ae37953bc53e9e7158cac571fc
AlainBox <93340821+AlainBox@users.noreply.github.com>
2021-12-25 14:53:21 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+const age = '18'
+const isFemale = 'true'
+const driverStatus = 'designated'
+
+if (age >= 18){
+
+    console.log("You are old enough to enter.")
+} else{
+
+    console.log("You are not old enough to enter, sorry.")
+}
+
+if (isFemale == 'true'){
+
+    console.log("You are allowed inside for ladies night.")
+} else{
+
+    console.log("You are not allowed inside because of our ladies night.")
+}
+
+if (driverStatus == 'designated'){
+
+    console.log("You can drive home safely.")
+} else{
+
+    console.log("We will call you a cab, stay here.")
+}
+
